3d900ce047afaee526a0fab79e030314aa6029e1
dahans1 <san.dahan@ufl.edu>
2025-05-15 16:13:57 -0400

    Add testing for answer format

--- agent.py
+++ agent.py
@@ -27,7 +27,7 @@ def assistant(state: AgentState):
         Use this to search the web via DuckDuckGo
 """
     # system prompt provided to ensure agent answer is using the correct and expected format
-    sys_message = f"You are a general AI assistant with provided tools:\n{textual_description_of_tools} \n I will ask you a question. Report your thoughts, and finish your answer with the following template: [YOUR FINAL ANSWER]. YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. If you are asked for a string, don't use articles, neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string."
+    sys_message = f"You are a general AI assistant with provided tools:\n{textual_description_of_tools} \n I will ask you a question. Analyze the question and if you find an answer, your response should only have the following template: [YOUR FINAL ANSWER]. Do NOT include the opening and closing square brackets. Only include what's inside the square brackets. YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. If you are asked for a string, don't use articles, neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string."
     sys_msg = SystemMessage(content=sys_message)
 
     return {
@@ -47,3 +47,19 @@ def build_graph():
     builder.add_edge("tools", "assistant")
 
     return builder.compile()
+
+# test agent for expected answer format
+if __name__ == "__main__":
+    question = "What country had the least number of athletes at the 1928 Summer Olympics? If there's a tie for a number of athletes, return the first in alphabetical order. Give the IOC country code as your answer."
+    print(f"Answering question...{question}")
+
+    graph = build_graph()
+    messages = [HumanMessage(content=question)]
+    messages = graph.invoke({"messages": messages})
+    answer = messages["messages"][-1].content
+
+    if "</think>" in answer:
+        answer = answer.split("</think>", 1)[1].strip()
+    else:
+        answer = answer.strip()
+    print(answer)
