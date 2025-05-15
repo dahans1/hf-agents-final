a5078db63c4e3e0389d5e7c925c2cc41fba5ea73
dahans1 <san.dahan@ufl.edu>
2025-05-15 15:00:14 -0400

    Add tool to search web

--- agent.py
+++ agent.py
@@ -5,7 +5,16 @@ from langgraph.graph import START, StateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_ollama import ChatOllama
 
-tools = []
+from langchain_community.tools import DuckDuckGoSearchRun
+
+def web_search(query: str) -> str:
+    """
+    Use this to search the web via DuckDuckGo
+    """
+
+    return DuckDuckGoSearchRun().invoke(query)
+
+tools = [web_search]
 llm = ChatOllama(model="qwen3:32b")
 llm_with_tools = llm.bind_tools(tools)
 
@@ -13,8 +22,9 @@ class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
 
 def assistant(state: AgentState):
-    textual_description_of_toools="""
-    todo: add search_engine, and wiki_search
+    textual_description_of_tools="""
+    web_search(query: str) -> str:
+        Use this to search the web via DuckDuckGo
 """
     # system prompt provided to ensure agent answer is using the correct and expected format
     sys_message = f"You are a general AI assistant with provided tools:\n{textual_description_of_tools} \n I will ask you a question. Report your thoughts, and finish your answer with the following template: [YOUR FINAL ANSWER]. YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. If you are asked for a string, don't use articles, neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string."
